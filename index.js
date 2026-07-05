de8d9a85c0db51f55e9994c40dfb6a83df8e23e3
Fadwa <eyryzyey@gmail.com>
2026-07-06 00:37:33 +0100

    Fix global URL for the application

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 9012;
 
 // 🌍 متغير عام
-global.t = "https://virxi.vercel.app/";
+global.t = "https://virixi.vercel.app/";
 
 // 📁 مجلد الروتات
 const routesDir = path.join(__dirname, "routes");
